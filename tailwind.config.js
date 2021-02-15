6186d39286d206a29ea2b1b6f88454f45fa4fd18
Alex S <schieder@gbk-rae.de>
2021-02-15 13:05:27 +0100

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          hover: '#214E82',
+          DEFAULT: '#183B63',
+        },
+        secondary: {
+          hover: '#4FB9B5',
+          DEFAULT: '#5CC8C5',
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
