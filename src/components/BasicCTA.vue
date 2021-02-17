bae39cf3597c25b55c9b5016bbdf1f1514ec1e45
Alex S <schieder@gbk-rae.de>
2021-02-17 12:03:18 +0100

    Add BasicCTA component

---
+++ src/components/BasicCTA.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="bg-primary py-11 text-center">
+        <h2 class="font-medium text-secondary mb-5 text-4xl">{{ title }}</h2>
+        <!-- TODO: only show p if prop is submitted -->
+        <p class="text-white mb-6">{{ subtitle }}</p>
+        <router-link class="btn-primary" to="/bussgeldbescheid-pruefen">
+            Bussgeldbescheid Prüfung
+        </router-link>
+    </section>
+</template>
+<script>
+export default {
+    name: 'BasicCTA',
+    props: {
+        title: String,
+        subtitle: String,
+    }
+}
+</script>
+<style scoped>
+
+</style>
