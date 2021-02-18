66c613c8d8b084c465d13a30992738a38fd88145
Alex S <schieder@gbk-rae.de>
2021-02-18 17:49:17 +0100

    Add Process Site

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="process">
-    <h1>Wie funktioniert's</h1>
+    <div class="container">
+        <h1 class="text-center text-4xl md:text-left md:text-5xl font-light mb-6">Wie funktioniert's</h1>
+        <section class="mb-6" v-for="step in steps" :key="step.id">
+          <!-- TODO: Problem with Images -->
+          <img :src="step.image.src" :alt="step.image.alt">
+          <h2 class="text-center md:text-left font-medium text-secondary mb-5 text-3xl md:text-4xl">{{ step.title }}</h2>
+          <div v-html="step.text"></div>
+        </section>
+    </div>
+    <Warrants />
   </div>
 </template>
+<script>
+import stepsData from '../assets/data/process-steps.json'
+import Warrants from '../components/BasicWarrants.vue'
+
+export default {
+  name: 'Process',
+  components: {
+    Warrants,
+  },
+  data() {
+    return {
+      steps: stepsData,
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
