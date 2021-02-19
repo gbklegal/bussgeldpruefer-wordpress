755bd1b08269adb96b3d0041d0fee828b3fa1328
Alex S <schieder@gbk-rae.de>
2021-02-19 14:08:04 +0100

    Change styling and text

--- src/views/Process.vue
+++ src/views/Process.vue
@@ -2,24 +2,28 @@
   <div class="process">
     <div class="container">
         <h1 class="text-center text-4xl md:text-left md:text-5xl font-light mb-6">Wie funktioniert's</h1>
-        <section class="mb-6" v-for="step in steps" :key="step.id">
+        <section class="mb-6 grid" v-for="step in steps" :key="step.id">
           <!-- TODO: Problem with Images -->
+          <!-- TODO: change Json file -->
           <img :src="step.image.src" :alt="step.image.alt">
-          <h2 class="text-center md:text-left font-medium text-secondary mb-5 text-3xl md:text-4xl">{{ step.title }}</h2>
-          <div v-html="step.text"></div>
+          <h2 class="font-medium text-secondary mb-5 text-3xl md:text-4xl">{{ step.title }}</h2>
+          <div class="process-text" v-html="step.text"></div>
         </section>
     </div>
+    <CTA title="Kostenlose Prüfung ob sich ein Einspruch gegen Deinen Bußgeldbescheid lohnt." />
     <Warrants />
   </div>
 </template>
 <script>
 import stepsData from '../assets/data/process-steps.json'
 import Warrants from '../components/BasicWarrants.vue'
+import CTA from '../components/BasicCTA.vue'
 
 export default {
   name: 'Process',
   components: {
     Warrants,
+    CTA,
   },
   data() {
     return {
@@ -29,5 +33,27 @@ export default {
 }
 </script>
 <style scoped>
+  @media (min-width: 640px) {
+    section {
+      grid-template-areas:
+        'img title'
+        'img text';
 
+      grid-template-columns: auto 1fr;
+    }
+
+    img {
+      grid-area: img;
+      align-self: center;
+      margin-right: 2em;
+    }
+
+    h2 {
+      grid-area: title;
+    }
+
+    .process-text {
+      grid-area: text;
+    }
+  }
 </style>
